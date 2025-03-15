8c7fa6513a7662ea8d3ef86c9f59092aa082a0ef
ROHITH-M10 <rohithmemail@gmail.com>
2025-03-16 01:50:19 +0530

    pwa with ui code update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "c10ed0da4416f98b59c91a2a31dc3bac"
   }, {
     "url": "index.html",
-    "revision": "0.uqj3hkfft38"
+    "revision": "0.lfhvl7rjuco"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const pages = {
+  generalPages: {
+    "Amrita Hospitals Research Data Request": "https://research.amritahospitals.org/his-data-request",
+    "Sample1": "https://example.com/page1",
+    "Sample2": "https://example.com/page2"
+  },
+  domainOne: {
+    "NAFLD/MASLD": "https://research.amritahospitals.org/nafld/masld/new",
+    "Sample1": "https://example.com/page8"
+  }
+};
+
+const Dropdown = ({ title, items, onSelect }) => {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-      <div>
-        Hello World from vite-react
-      </div>
-  )
-}
+    <div className="dropdown">
+      <button className="dropbtn" onClick={() => setIsOpen(!isOpen)}>
+        {title}
+      </button>
+      {isOpen && (
+        <div className="dropdown-content">
+          {Object.entries(items).map(([name, url]) => (
+            <button
+              key={name}
+              className="dropdown-item"
+              onClick={() => {
+                onSelect(url);
+                setIsOpen(false);
+              }}
+            >
+              {name}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default App
+const App = () => {
+  const [selectedUrl, setSelectedUrl] = useState("");
+
+  return (
+    <div>
+      <div className="go-to-home-nav-bar">
+            <button onClick={() => setSelectedUrl("")}>Home</button>
+            <i className="fa fa-bars"></i>
+      </div>
+
+      <h2>PWA TITLE</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad officia quod
+        dolorum deleniti sit dignissimos.
+      </p>
+
+      {/* Image Gallery */}
+      <div className="gallery-grid">
+        <img src="/sample_image.png" alt="Sample" />
+        <img src="/sample_image.png" alt="Sample" />
+      </div>
+
+      {/* Dropdowns */}
+      <Dropdown title="General Pages" items={pages.generalPages} onSelect={setSelectedUrl} />
+      <Dropdown title="Module One Pages" items={pages.domainOne} onSelect={setSelectedUrl} />
+
+      {/* iFrame to display selected page */}
+      {selectedUrl && (
+        <div className="iframe-container">
+          <iframe src={selectedUrl} title="content-frame"></iframe>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
