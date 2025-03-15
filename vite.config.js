d6a696bb681da47ba6d8929751aa6ee67c21a16c
ROHITH-M10 <rohithmemail@gmail.com>
2025-03-16 01:06:20 +0530

    added PWA for test

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/PWA_TEST_3",
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      base: "/PWA_TEST_3/",
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: "Amrita PWA",
+        short_name: "Amrita PWA",
+        description: "PWA App for Amrita Hospital",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/PWA_TEST_3/",
+        icons: [
+          {
+            src: "/PWA_TEST_3/icons/favicon.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/PWA_TEST_3/icons/favicon.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ],
+  base: "/PWA_TEST_3/"
+});
