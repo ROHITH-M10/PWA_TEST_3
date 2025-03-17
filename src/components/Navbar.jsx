505ff81a40d39102e85e62f58b112dcf1270b16d
ROHITH-M10 <rohithmemail@gmail.com>
2025-03-17 10:48:06 +0530

    nav options, hide admin nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Navbar({ onHomeClick }) {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("AIMS Metaflo");
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleSelect = (option) => {
+    setSelectedOption(option);
+    setIsDropdownOpen(false); // Close dropdown after selection
+  };
+
   return (
     <div className="go-to-home-nav-bar">
       <button onClick={onHomeClick} className="home-button">
         Home
       </button>
-      <i onClick={onHomeClick} className="fa fa-bars"></i>
+      <div className="menu-container">
+        <i onClick={toggleDropdown} className="fa fa-bars"></i>
+
+        {isDropdownOpen && (
+          <div className="dropdown-menu">
+            <div 
+              className={`dropdown-item ${selectedOption === "Metaflo" ? "selected" : ""}`} 
+              onClick={() => handleSelect("Metaflo")}
+            >
+              Metaflo
+            </div>
+            <div 
+              className={`dropdown-item ${selectedOption === "AIMS Metaflo" ? "selected" : ""}`} 
+              onClick={() => handleSelect("AIMS Metaflo")}
+            >
+              AIMS Metaflo
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
