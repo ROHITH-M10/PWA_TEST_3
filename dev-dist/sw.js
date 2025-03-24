19c9d88423e823a95d59c269b90ce6dba3b59683
ROHITH-M10 <rohithmemail@gmail.com>
2025-03-24 14:23:39 +0530

    line-height update text

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "c10ed0da4416f98b59c91a2a31dc3bac"
   }, {
     "url": "index.html",
-    "revision": "0.9kmse1l8aqo"
+    "revision": "0.5j4j06n72p8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
