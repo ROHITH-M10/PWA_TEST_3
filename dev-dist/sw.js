9af5841592fe7240f4af7b9e7ea865a2b2606a15
ROHITH-M10 <rohithmemail@gmail.com>
2025-03-25 16:05:13 +0530

    commit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "c10ed0da4416f98b59c91a2a31dc3bac"
   }, {
     "url": "index.html",
-    "revision": "0.mddo66kujug"
+    "revision": "0.ojbqaeslmd8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
