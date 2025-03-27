838d3f8e72a221b9b5cf0b5064712756348d4d96
ROHITH-M10 <rohithmemail@gmail.com>
2025-03-27 12:43:54 +0530

    proxy server test

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: "/PWA_TEST_3/",  //  Set the correct base for GitHub Pages
+  base: "/PWA_TEST_3/",  // Set the correct base for GitHub Pages
   plugins: [
     react(),
     VitePWA({
@@ -18,7 +18,7 @@ export default defineConfig({
         theme_color: "#ffffff",
         background_color: "#ffffff",
         display: "standalone",
-        start_url: "/PWA_TEST_3/", //  Ensure correct start URL
+        start_url: "/PWA_TEST_3/", // Ensure correct start URL
         icons: [
           {
             src: "/PWA_TEST_3/icons/favicon.png", // Make sure this file exists
@@ -33,5 +33,50 @@ export default defineConfig({
         ]
       }
     })
-  ]
+  ],
+  server: {
+    proxy: {
+      
+      "/login": {
+        target: "https://research.amritahospitals.org",
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/proxy-webforms/, "/app"),
+      },
+      "/assets": {
+        target: "https://research.amritahospitals.org",
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/proxy-webforms/, "/app"),
+      },
+
+      "/app": {
+        target: "https://research.amritahospitals.org",
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/proxy-webforms/, "/app"),
+      },
+
+      "/website_script.js": {
+        target: "https://research.amritahospitals.org",
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/proxy-webforms/, "/app"),
+      },
+
+      "/api": {
+        target: "https://research.amritahospitals.org",
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/proxy-webforms/, "/app"),
+      },
+
+
+      "/proxy-registries": {
+        target: "https://research-int.amritahospitals.org",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/proxy-registries/, ""),
+      },
+      "/proxy-staging": {
+        target: "https://test-ahis-l1.amrita.edu",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/proxy-staging/, ""),
+      }
+    }
+  }
 });
